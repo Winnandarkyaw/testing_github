904b262669dea5c19699d109c68d8ce413cffc38
Win Nandar Kyaw <winnandarkyaw2000@gmail.com>
2021-08-12 12:30:29 +0630

    update email feature

--- email.php
+++ email.php
@@ -3,10 +3,8 @@ echo "sending email";
 echo "another email send";
 echo "Third line from cli";
 echo "Third line from online";
-
 echo "Fourth line from cli";
-
 echo "Forth line from online";
 echo "Fifth line from online";
-
+echo "Fifth line from cli";
 ?>
